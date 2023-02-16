0a50a4da7aa20a1ae8c74ce57ac5fa19bae9db78
Nihat-Yildirim <nihat.yilldirim@gmail.com>
2023-02-16 23:23:52 +0300

    Veri tabanı nesnelerinin default CRUD işlermleri service içerisinde yazıldı

---
+++ Core/Business/IDefaultBusinessCrudMethods.cs
@@ -0,0 +1,21 @@
+﻿using Core.Entities;
+using Core.Utilities.Results.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Business
+{
+    public interface IDefaultBusinessCrudMethods<TEntity>
+        where TEntity : class,IEntity, new()
+    {
+        IDataResult<List<TEntity>> GetAll();
+        IDataResult<TEntity> GeyById(int id);
+        IResult Add(TEntity entity);
+        IResult Update(TEntity entity);
+        IResult Delete(TEntity entity);
+    }
+}
