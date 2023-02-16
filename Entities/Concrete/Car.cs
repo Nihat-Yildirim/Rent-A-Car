9fd988936e3dc3748cd786f7db6ab0a59b2ed403
Nihat-Yildirim <nihat.yilldirim@gmail.com>
2023-02-16 19:26:49 +0300

    Temel proje nesneleri oluşturuldu

---
+++ Entities/Concrete/Car.cs
@@ -0,0 +1,20 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete
+{
+    public class Car : IEntity
+    {
+        public int Id { get; set; }
+        public int BrandId { get; set; }
+        public int ColorId { get; set; }
+        public DateTime ModelYear { get; set; }
+        public float DailyPrice { get; set; }
+        public string Description { get; set; }
+
+    }
+}
