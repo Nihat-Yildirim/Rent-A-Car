beca1073071c6fd04fd9e7c8d9d663784b748227
Nihat-Yildirim <nihat.yilldirim@gmail.com>
2023-02-16 21:38:37 +0300

    Projeye arabaları detaylı getirmeye yarayan GetCarDetails metodu eklendi

--- DataAccess/Concrete/EntityFramework/EfCarDal.cs
+++ DataAccess/Concrete/EntityFramework/EfCarDal.cs
@@ -1,6 +1,7 @@
 ﻿using Core.DataAccess.EntityFremawork;
 using DataAccess.Abstract;
 using Entities.Concrete;
+using Entities.DTOs;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,7 +10,26 @@ using System.Threading.Tasks;
 
 namespace DataAccess.Concrete.EntityFramework
 {
-    public class EfCarDal : EfEntityRepositoryBase<Car,RentACarContext>,ICarDal
+    public class EfCarDal : EfEntityRepositoryBase<Car, RentACarContext>, ICarDal
     {
+        public List<CarDetailDto> GetCarDetails()
+        {
+            using(RentACarContext context = new RentACarContext()) 
+            {
+                var result = from car in context.Cars
+                             join color in context.Colors
+                             on car.ColorId equals color.Id
+                             join brand in context.Brands
+                             on car.BrandId equals brand.Id
+                             select new CarDetailDto
+                             {
+                                 CarName = car.CarName,
+                                 BrandName = brand.BrandName,
+                                 ColorName = color.ColorName,
+                                 DailyPrice = car.DailyPrice
+                             };
+                return result.ToList();
+            }
+        }
     }
 }
