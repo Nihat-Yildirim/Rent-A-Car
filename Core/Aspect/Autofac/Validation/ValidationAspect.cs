407d439676cf744281290f1456279953a511b1ef
Nihat-Yildirim <nihat.yilldirim@gmail.com>
2023-02-19 23:51:13 +0300

    Validation toolun aspect hali yazıldı , Autoface AspectInterceptorSelector seçici olarak eklendi

---
+++ Core/Aspect/Autofac/Validation/ValidationAspect.cs
@@ -0,0 +1,36 @@
+﻿using Castle.DynamicProxy;
+using Core.CrossCuttingConcerns.Validation;
+using Core.Utilities.Interceptors;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Aspect.Autofac.Validation
+{
+    public class ValidationAspect : MethodInterception
+    {
+        private Type _validatortype;
+        public ValidationAspect(Type validatortype)
+        {
+            if(!typeof(IValidator).IsAssignableFrom(validatortype))
+            {
+                throw new System.Exception("Bu bir doğrulama sını değil");
+            }
+            _validatortype = validatortype;
+        }
+
+        public override void OnBefore(IInvocation ınvocation)
+        {
+            var validator = (IValidator)Activator.CreateInstance(_validatortype);
+            var entityType = _validatortype.BaseType.GetGenericArguments()[0];
+            var entities = ınvocation.Arguments.Where(t => t.GetType() == entityType);
+            foreach( var entity in entities )
+            {
+                ValidationTool.Validate(validator, entity);
+            }
+        }
+    }
+}
