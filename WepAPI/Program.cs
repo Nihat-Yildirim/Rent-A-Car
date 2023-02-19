3e1d266501881880e09d9e11e9bc803f06f3123c
Nihat-Yildirim <nihat.yilldirim@gmail.com>
2023-02-19 22:09:56 +0300

    Projeye AutoFac desteği getirildi

--- WepAPI/Program.cs
+++ WepAPI/Program.cs
@@ -1,3 +1,7 @@
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using Business.DependencyResolvers.Autofac;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory())
+    .ConfigureContainer<ContainerBuilder>(builder =>
+    {
+        builder.RegisterModule(new AutoFacBusinessModule());
+    });
 
 var app = builder.Build();
 
