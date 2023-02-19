424cab64cffac60934ae4933844fc0c66ec61b6c
Nihat-Yildirim <nihat.yilldirim@gmail.com>
2023-02-19 23:31:04 +0300

    validation kuralları yazıldı ve projeye AOP desteği için gerekli olan Interceptor kodları yazıldı

---
+++ Business/ValidationRules/FluentValidation/UserValidator.cs
@@ -0,0 +1,29 @@
+﻿using Entities.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.ValidationRules.FluentValidation
+{
+    public class UserValidator : AbstractValidator<User>
+    {
+        public UserValidator()
+        {
+            RuleFor(u => u.FirstName).NotEmpty();
+            RuleFor(u => u.LastName).NotEmpty();
+            RuleFor(u => u.EMail).NotEmpty();
+            RuleFor(u => u.Password).NotEmpty();
+            RuleFor(u => u.FirstName).MinimumLength(3);
+            RuleFor(u => u.FirstName).MaximumLength(100);
+            RuleFor(u => u.LastName).MinimumLength(2);
+            RuleFor(u => u.LastName).MaximumLength(150);
+            RuleFor(u => u.EMail).MinimumLength(15);
+            RuleFor(u => u.EMail).MinimumLength(200);
+            RuleFor(u => u.Password).MinimumLength(6);
+            RuleFor(u => u.Password).MaximumLength(25);
+        }
+    }
+}
---
+++ Core/Utilities/Interceptors/MethodInterception.cs
@@ -0,0 +1,42 @@
+﻿using Castle.DynamicProxy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Utilities.Interceptors
+{
+    public class MethodInterception : MethodInterceptionBaseAttribute
+    {
+        public virtual void OnBefore(IInvocation ınvocation) { }
+        public virtual void OnAfter(IInvocation ınvocation) { }
+        public virtual void OnException(IInvocation ınvocation, Exception exception) { }
+        public virtual void OnSuccess(IInvocation ınvocation) { }
+
+        public override void Intercept(IInvocation ınvocation)
+        {
+            bool isSuccess = true;
+            OnBefore(ınvocation);
+            try
+            {
+                ınvocation.Proceed();
+            }
+            catch (Exception exception)
+            {
+                isSuccess = false;
+                OnException(ınvocation, exception);
+                throw;
+            }
+            finally
+            {
+                if (isSuccess)
+                {
+                    OnSuccess(ınvocation);
+                }
+            }
+            OnAfter(ınvocation);
+        }
+
+    }
+}
